f766c360cd4f7c762f716527bc9a80032bedb461
Hayri Can Akyel <cakyel@gmail.com>
2016-11-23 12:53:58 +0300

    Initial Commit

---
+++ ONAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ONAPP
+//
+//  Created by Hayri Can Akyel on 23.11.2016.
+//  Copyright © 2016 Hayri Can Akyel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
